2208d51be21123597a4b5df77d9366dd3723668c
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-08-04 19:19:33 -0400

    fix style error

---
+++ app/NavMenu.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from './NavMenu.module.css'
+
+export default function NavMenu() {
+    return (
+        <nav className ={styles.nav}>
+            <Link href={'/'}>
+                <Image
+                    src="/logo.svg"
+                    width={216}
+                    height={30}
+                    alt="Nextspace Logo"
+                />
+            </Link>
+            <ul className={styles.links}>
+                <li>
+                    <Link href={'/about'}>About</Link>
+                </li>
+                 <li>
+                    <Link href={'/blog'}>Blogs</Link>
+                </li>
+                 <li>
+                    <Link href={'/users'}>Users</Link>
+                </li>
+
+            </ul>
+
+        </nav>
+
+
+    )
+}
