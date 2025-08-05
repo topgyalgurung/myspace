af07c392f9102a4934cf378146640bc6525b533d
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-08-04 20:27:59 -0400

    add generateStaticParams for add optimization, ISG with revalidate

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 //caching 
 // export const dynamic = 'force-dynamic' // always fetches latest data 
-export const revalidate = 420; // data updated occassionally, update every 420 secs
+export const revalidate = 420; // data updated occassionally, update every 420 secs (rebuild on demand)
 // we can also control caching at fetch level 
     // fetch(, {cache: 'no-store' or  'force-cache'}). // no-store = never cache, force-cache= always
 
@@ -17,6 +17,17 @@ interface Props{
     params: {slug: string}; // slug as url param
 }
 
+// find dynamic data and render in advance, ideal for dynamic data does not change often 
+// recommend regular server side rendering and generatestaticparams as additional optimization for certain types of data 
+// and use revalidate to rebuild on demand, this incremental static generation - best of both world 
+export async function generateStaticParams(){
+     const posts:Post[] = await fetch('http://localhost:3000/api/content').then(
+        (res) => res.json()
+    );
+    return posts.map((post) =>({
+        slug:post.slug,
+    })) 
+}
 export default async function BlogPostsPage({params}: Props){
     // dedupled 
     const posts:Post[] = await fetch('http://localhost:3000/api/content').then(
@@ -31,7 +42,6 @@ export default async function BlogPostsPage({params}: Props){
         <div>
             <h1> {post.title}</h1>
             <p> {post.content}</p>
-
         </div>
     )
 }
