c6697459d642908a88ac2323e6646c0eec632b7e
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-08-06 02:04:00 -0400

    feat: follow unfollow user, followButton server component, followClient w useTransition, router, derived state

---
+++ app/components/FollowButton/FollowClient.tsx
@@ -0,0 +1,64 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useTransition, useState } from "react";
+
+interface Props{
+    targetUserId: string;
+    isFollowing: boolean;
+}
+export default function FollowClient({targetUserId, isFollowing}: Props){
+    const router = useRouter();
+
+    const [isPending, startTransition] = useTransition();
+    const [isFetching, setIsFetching ] = useState(false);
+    // derived state 
+    const isMutating = isFetching || isPending;
+
+    const follow = async() => {
+        setIsFetching(true);
+
+        // sent targetUserId in the request body to create new relation in the db 
+        const res = await fetch('/api/follow', {
+            method: "POST",
+            body: JSON.stringify({targetUserId}),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+        setIsFetching(false);
+        console.log(res);
+
+        startTransition(() =>{
+            // refresh the current route
+            // make a new request to server for the route
+            // refetches data requests and rerender server components
+            // sends updated react server component payload to client
+            // client merges the payload without losing unaffected client side react state 
+            router.refresh();
+        })
+    }
+    const unfollow = async() =>{
+        setIsFetching(true);
+
+        const res = await fetch(`/api/follow?targetUserId=${targetUserId}`,{
+            method:'DELETE',  
+        });
+        setIsFetching(false);
+        startTransition(() => router.refresh());
+    }
+
+    if(isFollowing){
+        return(
+            <button onClick={unfollow}>
+                {!isMutating ? 'Unfollow': "..."}
+            </button>
+        )
+    }else{
+        return(
+            <button onClick={follow}>
+                {!isMutating ? 'Follow': "..."}
+            </button>
+        )
+    }
+
+}
